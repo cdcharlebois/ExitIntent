e03c0eaa9c8f3d0dbde796baefb10343cf2139ee
Conner <cdcharlebois@gmail.com>
2016-11-03 17:00:36 -0400

    borken 'around' function

--- src/ExitIntent/widget/ExitIntent.js
+++ src/ExitIntent/widget/ExitIntent.js
@@ -1,22 +1,38 @@
 define([
     "dojo/_base/declare",
     "mxui/widget/_WidgetBase",
+    "dojo/aspect",
 
-    "mxui/dom",
-    "dojo/dom",
-    "dojo/dom-prop",
-    "dojo/dom-geometry",
-    "dojo/dom-class",
-    "dojo/dom-style",
-    "dojo/dom-construct",
-    "dojo/_base/array",
-    "dojo/_base/lang",
-    "dojo/text",
-    "dojo/html",
-    "dojo/_base/event",
+    // "mxui/dom",
+    // "dojo/dom",
+    // "dojo/dom-prop",
+    // "dojo/dom-geometry",
+    // "dojo/dom-class",
+    // "dojo/dom-style",
+    // "dojo/dom-construct",
+    // "dojo/_base/array",
+    "dojo/_base/lang"
+    // "dojo/text",
+    // "dojo/html",
+    // "dojo/_base/event",
 
 
-], function (declare, _WidgetBase, dom, dojoDom, dojoProp, dojoGeometry, dojoClass, dojoStyle, dojoConstruct, dojoArray, dojoLang, dojoText, dojoHtml, dojoEvent) {
+], function (declare,
+_WidgetBase,
+aspect,
+// dom,
+// dojoDom,
+// dojoProp,
+// dojoGeometry,
+// dojoClass,
+// dojoStyle,
+// dojoConstruct,
+// dojoArray,
+dojoLang
+// dojoText,
+// dojoHtml,
+// dojoEvent
+) {
     "use strict";
 
     return declare("ExitIntent.widget.ExitIntent", [ _WidgetBase ], {
@@ -26,40 +42,83 @@ define([
         _handles: null,
         _contextObj: null,
 
-        constructor: function () {
-            this._handles = [];
-        },
 
         postCreate: function () {
             logger.debug(this.id + ".postCreate");
+            // window.hooked = window.hooked || false
+            var self = this
+            // console.log(this.mxform.navigateTo)
+            // console.log(this)
+            // var self = this;
+            // if (!window.hooked){
+            //working with before
+            // self.handle = aspect.before(
+            //   self.mxform, // target
+            //   "navigateTo", // method name
+            //   function(){
+            //     console.log(self._contextObj)
+            //     if (!self._contextObj) return
+            //     // var c = confirm('do you want to save to this customer? (If you select "no", your changes will not be saved.)')
+            //     // if (c) {
+            //     //   self._runSaveMicroflow(self._contextObj)
+            //     // }
+            //     // if the object is modified
+            //     mx.ui.confirmation({
+            //       content:"do you want to save to this customer? (If you select 'no', your changes will not be saved.)",
+            //       proceed:"Yes, save",
+            //       cancel:"No, discard my changes",
+            //       handler: function(){
+            //         self._runSaveMicroflow(self._contextObj)
+            //       }
+            //     })
+            //   }
+            // );
+            aspect.around(self.mxform, "navigateTo", dojoLang.hitch(self, self._aroundFunc));
+
+              // window.hooked = true
+            // }
         },
 
-        update: function (obj, callback) {
-            logger.debug(this.id + ".update");
-
-            this._contextObj = obj;
-            this._updateRendering(callback);
+        _aroundFunc: function(originalNav){
+          console.log('hi');
+          return function(){
+            // doing something before the original call
+            var deferred = originalNav.apply(self, arguments);
+            // doing something after the original call
+            return deferred;
+          };
         },
 
-        resize: function (box) {
-          logger.debug(this.id + ".resize");
+        update: function(obj, cb){
+          if (obj){
+            this._contextObj = obj
+          }
+          cb();
         },
 
-        uninitialize: function () {
-          logger.debug(this.id + ".uninitialize");
+        uninitialize: function() {
+          this.handle.remove();
         },
 
-        _updateRendering: function (callback) {
-            logger.debug(this.id + "._updateRendering");
-
-            if (this._contextObj !== null) {
-                dojoStyle.set(this.domNode, "display", "block");
-            } else {
-                dojoStyle.set(this.domNode, "display", "none");
+        _runSaveMicroflow: function(obj){
+          if (!obj) return;
+          console.log('saving ' + obj.getGuid())
+          mx.data.action({
+            params: {
+              actionname: 'TestSuite.IVK_SaveForgottenPerson',
+              applyto: 'selection',
+              guids: [obj.getGuid()]
+            },
+            callback: function(res){
+              console.log('success')
+            },
+            error: function(err){
+              console.log('err')
             }
-
-            mendix.lang.nullExec(callback);
+          })
         }
+
+
     });
 });
 
