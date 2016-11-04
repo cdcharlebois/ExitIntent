c1705686eac33b1ab73063a43b0ddcaa4f12f3ca
Conner <cdcharlebois@gmail.com>
2016-11-04 10:15:54 -0400

    added check for changes

--- src/ExitIntent/widget/ExitIntent.js
+++ src/ExitIntent/widget/ExitIntent.js
@@ -1,7 +1,5 @@
 define([
-    "dojo/_base/declare",
-    "mxui/widget/_WidgetBase",
-    "dojo/aspect",
+    "dojo/_base/declare", "mxui/widget/_WidgetBase", "dojo/aspect",
 
     // "mxui/dom",
     // "dojo/dom",
@@ -15,11 +13,9 @@ define([
     // "dojo/text",
     // "dojo/html",
     // "dojo/_base/event",
-	"ExitIntent/widget/lib/ConfirmationDialog2"
+    "ExitIntent/widget/lib/ConfirmationDialog2"
 
-], function (declare,
-_WidgetBase,
-aspect,
+], function(declare, _WidgetBase, aspect,
 // dom,
 // dojoDom,
 // dojoProp,
@@ -32,119 +28,117 @@ dojoLang,
 // dojoText,
 // dojoHtml,
 // dojoEvent
-confirmationDialog2
-) {
+confirmationDialog2) {
     "use strict";
 
-    return declare("ExitIntent.widget.ExitIntent", [ _WidgetBase ], {
-
+    return declare("ExitIntent.widget.ExitIntent", [_WidgetBase], {
 
         // Internal variables.
         _handles: null,
         _contextObj: null,
 
-
-        postCreate: function () {
-            logger.debug(this.id + ".postCreate");
-            // window.hooked = window.hooked || false
-            //var self = this;
-            // console.log(this.mxform.navigateTo)
+        postCreate: function() {
             // console.log(this)
-            // var self = this;
-            // if (!window.hooked){
-            //working with before
-            // self.handle = aspect.before(
-            //   self.mxform, // target
-            //   "navigateTo", // method name
-            //   function(){
-            //     console.log(self._contextObj)
-            //     if (!self._contextObj) return
-            //     // var c = confirm('do you want to save to this customer? (If you select "no", your changes will not be saved.)')
-            //     // if (c) {
-            //     //   self._runSaveMicroflow(self._contextObj)
-            //     // }
-            //     // if the object is modified
-            //     mx.ui.confirmation({
-            //       content:"do you want to save to this customer? (If you select 'no', your changes will not be saved.)",
-            //       proceed:"Yes, save",
-            //       cancel:"No, discard my changes",
-            //       handler: function(){
-            //         self._runSaveMicroflow(self._contextObj)
-            //       }
-            //     })
-            //   }
-            // );
+            logger.debug(this.id + ".postCreate");
+
             this.handle = aspect.around(window.mx.router, "openFormInContent", dojoLang.hitch(this, this._aroundFunc));
 
-              // window.hooked = true
-            // }
         },
 
-        _aroundFunc: function(origOpenFormInContent){
-          console.log('hi');
-          var self = this;
-		  var confirm2 = function(args) {
-		                          new confirmationDialog2({
-		                              content: args.content,
-		                              proceed: args.proceed || this.translate("mxui.widget.DialogMessage", "ok"),
-		                              cancel: args.cancel || this.translate("mxui.widget.DialogMessage", "cancel"),
-		                              handler: args.handler,
-									  cancelHandler: args.cancelHandler
-		                          }).show();
-		                      };
+        _aroundFunc: function(origOpenFormInContent) {
+            // console.log('hi');
+            var self = this;
+            var confirm2 = function(args) {
+                new confirmationDialog2({
+                    content: args.content,
+                    proceed: args.proceed || this.translate("mxui.widget.DialogMessage", "ok"),
+                    cancel: args.cancel || this.translate("mxui.widget.DialogMessage", "cancel"),
+                    handler: args.handler,
+                    cancelHandler: args.cancelHandler
+                }).show();
+            };
 
-          return function(){
-            var origNav = origOpenFormInContent;
-            var args = arguments;
-            var theWidget = self;
-            var theRouter = this;
+            return function() {
+                var origNav = origOpenFormInContent;
+                var args = arguments;
+                var theWidget = self;
+                var theRouter = this;
 
-            confirm2({
-                   content:"do you want to save to this customer? (If you select 'no', your changes will not be saved.)",
-                   proceed:"Yes, save",
-                   cancel:"No, discard my changes",
-                   handler: function(){
-                     origNav.apply(theRouter, args);
-                     theWidget._runSaveMicroflow(theWidget._contextObj)
-				 	},
-				   cancelHandler: function() {
-					 console.log("cancel handler");
-					 origNav.apply(theRouter, args);
-				   }
-             })
-            return;
-          };
+                // check for changes
+                var objectsOnPage = self.mxform._formData._getObjectsFromProviders()
+                ,   objectsChanged = objectsOnPage.filter(function(o){return !self._isEmptyObject(mx.data.getChanges(o._guid))})
+
+                if (objectsChanged.length > 0){
+                  confirm2({
+                      content: "do you want to save to this customer? (If you select 'no', your changes will not be saved.)",
+                      proceed: "Yes, save",
+                      cancel: "No, discard my changes",
+                      handler: function() {
+                          origNav.apply(theRouter, args);
+                          theWidget._commitChanges(objectsChanged)
+                      },
+                      cancelHandler: function() {
+                          console.log("cancel handler");
+                          origNav.apply(theRouter, args);
+                      }
+                  })
+                }
+                else {
+                  origNav.apply(theRouter, args);
+                }
+                return;
+            };
         },
 
-        update: function(obj, cb){
-          if (obj){
-            this._contextObj = obj
-          }
-          cb();
+        update: function(obj, cb) {
+            if (obj) {
+                this._contextObj = obj
+            }
+            cb();
         },
 
         uninitialize: function() {
-          this.handle.remove();
+            this.handle.remove();
         },
 
-        _runSaveMicroflow: function(obj){
-          if (!obj) return;
-          console.log('saving ' + obj.getGuid())
-          mx.data.action({
-            params: {
-              actionname: 'TestSuite.IVK_SaveForgottenPerson',
-              applyto: 'selection',
-              guids: [obj.getGuid()]
-            },
-            callback: function(res){
+        _commitChanges: function(objects){
+          mx.data.commit({
+            mxobjs: objects,
+            callback: function() {
               console.log('success')
             },
-            error: function(err){
-              console.log('err')
+            error: function(err) {
+              console.log(err)
             }
           })
-        }
+        },
 
+        // _runSaveMicroflow: function(obj) {
+        //     if (!obj)
+        //         return;
+        //     console.log('saving ' + obj.getGuid())
+        //     mx.data.action({
+        //         params: {
+        //             actionname: 'TestSuite.IVK_SaveForgottenPerson',
+        //             applyto: 'selection',
+        //             guids: [obj.getGuid()]
+        //         },
+        //         callback: function(res) {
+        //             console.log('success')
+        //         },
+        //         error: function(err) {
+        //             console.log('err')
+        //         }
+        //     })
+        // },
+
+        _isEmptyObject: function(obj){
+            for(var prop in obj) {
+              if(obj.hasOwnProperty(prop))
+              return false;
+            }
+            return JSON.stringify(obj) === JSON.stringify({});
+        }
 
     });
 });
